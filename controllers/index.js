072c5f88abdfa9bcb2fbf415e18b1c0d79addbca
Jamie <hcmlinj@gmail.com>
2017-06-03 23:06:12 +0800

    jwt verify and decode

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@
 const fs = require('fs')
 
 module.exports = async ctx => {
+    ctx.log.info(ctx.state.user)
     ctx.log.info(ctx.request.host)
     ctx.body = fs.readFileSync('index.html').toString()
 }
